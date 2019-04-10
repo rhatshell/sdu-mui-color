14d8bfeeb692c395343cbc35d42ef80e679770d7
Robert Hamilton <Robert.Hamilton@shell.com>
2019-04-10 12:42:07 -0500

    Fix port number

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useStaticAssets(resolve(__dirname, '../dist/ui'));
-  await app.listen(process.env.PORT || 3001);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
