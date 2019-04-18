748a0c23b8f60db91874e9c5f2dfa58e75088886
Robert Hamilton <Robert.Hamilton@shell.com>
2019-04-18 10:01:49 -0500

    Log all requests

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
+import * as os from 'os';
 import { resolve } from 'path';
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { logger: new Logger() });
+  app.use((req, res, next) => {
+    const url = req.protocol + '://' + req.get('host') + req.originalUrl;
+    Logger.log(`Host: ${os.hostname}, Url: ${url}`);
+
+    next();
+  });
   app.useStaticAssets(resolve(__dirname, '../dist/ui'));
   await app.listen(process.env.PORT || 3000);
 }
